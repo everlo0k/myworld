a71697405ba894df63f57a48a1a767d4a6c04288
everlo0k <everlook@126.com>
2014-03-28 16:42:13 +0800

    init

---
+++ src/myworld/com/bigears/MyFirstGitHub.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2001-2014 Bidlink(Beijing) E-Biz Tech Co.,Ltd.
+ * All rights reserved.
+ * 必联（北京）电子商务科技有限公司 版权所有
+ * <p>MyFirstGitHub.java</p>
+ */
+package myworld.com.bigears;
+
+/**
+ * @Project    : myworld
+ * @ClassName  : myworld.com.bigears.MyFirstGitHub
+ * @Description: GitHub test
+ * @Author     : shenyang
+ * @Date  	   : 2014年3月28日 下午4:36:00 
+ */
+public class MyFirstGitHub {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
