f330f9f3faf43334d47e78cdeeb8f0b88911a4d1
everlo0k <everlook@126.com>
2014-09-10 14:57:45 +0800

    modify

--- src/com/bigears/inetaddress/MyInetAddress.java
+++ src/com/bigears/inetaddress/MyInetAddress.java
@@ -6,28 +6,52 @@
  */
 package com.bigears.inetaddress;
 
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
 import java.net.UnknownHostException;
 
 /**
  * @Project : myworld
  * @ClassName : com.bigears.inetaddress.MyInetAddress
- * @Description: InetAddress test
+ * @Description: InetAddress InetSocketAddress test
  * @Author : shenyang
  * @Date : 2014年8月27日 下午4:06:50
  */
 public class MyInetAddress {
 
 	public static void main(String[] args) {
-		
+
 		try {
 			InetAddress address = InetAddress.getLocalHost();
 			System.out.println("主机名：" + address.getHostName() + ", ip地址：" + address.getHostAddress());
-			InetAddress add = InetAddress.getByName("d-PC9");  
-			System.out.println(add.getHostAddress());  
+			InetAddress add = InetAddress.getByName("d-PC9");
+			System.out.println(add.getHostAddress());
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
+
+		final Socket socket = new Socket();
+		SocketAddress address = new InetSocketAddress("www.fortify.net", 443);
+		try {
+			socket.connect(address);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}// 连接远程主机
+		Thread reader = new Thread() {
+			@Override
+			public void run() {
+				try {
+					byte[] buffer = new byte[512];
+					socket.getInputStream().read(buffer);
+				} catch (Exception ex) {
+
+				}
+			}
+		};
+		reader.start();
 
 	}
 
