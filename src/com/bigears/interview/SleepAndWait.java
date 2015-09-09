ffb4ba0b2757a54c2774ff2ef8a6a17a03f2235c
everlook <everlook@126.com>
2015-09-09 22:37:00 +0800

    init code

---
+++ src/com/bigears/interview/SleepAndWait.java
@@ -0,0 +1,13 @@
+package com.bigears.interview;
+
+public class SleepAndWait {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
