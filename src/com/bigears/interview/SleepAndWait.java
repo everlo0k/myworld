0afacf137da5e9b09a2a42865e89ffa7e1dec5f6
everlook <everlook@126.com>
2015-09-10 19:54:56 +0800

    modify

--- src/com/bigears/interview/SleepAndWait.java
+++ src/com/bigears/interview/SleepAndWait.java
@@ -6,8 +6,49 @@ public class SleepAndWait {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("lalalala");
+		new Thread(new Thread1()).start();
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		new Thread(new Thread2()).start();
+	}
 
+	private static class Thread1 implements Runnable {
+
+		public void run() {
+			synchronized (SleepAndWait.class) {
+				System.out.println("Thread1 will be wait");
+				try {
+					// 调用wait()方法，线程会放弃对象锁，进入等待此对象的等待锁定池
+					SleepAndWait.class.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("Thread1 is running again");
+				System.out.println("Thread1 is end");
+			}
+		}
+
+	}
+
+	private static class Thread2 implements Runnable {
+
+		public void run() {
+			synchronized (SleepAndWait.class) {
+				System.out.println("Thread2 is running");
+				// 只有针对此对象调用notify()方法后本线程才进入对象锁定池准备获取对象锁进入运行状态。
+				SleepAndWait.class.notify();
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("Thread2 is end");
+			}
+		}
 	}
 
 }
