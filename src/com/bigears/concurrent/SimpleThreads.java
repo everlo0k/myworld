30ab93b09211f70597b41b90cd8d09bd6297f04f
everlo0k <everlook@126.com>
2015-09-29 15:12:59 +0800

    init

--- src/com/bigears/concurrent/SimpleThreads.java
+++ src/com/bigears/concurrent/SimpleThreads.java
@@ -12,7 +12,7 @@ package com.bigears.concurrent;
  */
 public class SimpleThreads {
 	// Display a message, preceded by
-	// the name of the current thread
+	// the name of the current thread 
 	static void threadMessage(String message) {
 		String threadName = Thread.currentThread().getName();
 		System.out.format("%s: %s%n", threadName, message);
