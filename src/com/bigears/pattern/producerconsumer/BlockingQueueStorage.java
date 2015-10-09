f365a24a5eb22e9565498e5801816aa82fc53cd8
everlo0k <everlook@126.com>
2015-10-09 11:51:07 +0800

    modify

--- src/com/bigears/pattern/producerconsumer/BlockingQueueStorage.java
+++ src/com/bigears/pattern/producerconsumer/BlockingQueueStorage.java
@@ -5,34 +5,86 @@ package com.bigears.pattern.producerconsumer;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
- * @Project    : myworld
- * @ClassName  : com.bigears.pattern.producerconsumer.BlockingQueueStorage
- * @Description: TODO
- * @Author     : shenyang
- * @Date  	   : 2015年10月8日 下午5:41:35 
+ * @Project : myworld
+ * @ClassName : com.bigears.pattern.producerconsumer.BlockingQueueStorage
+ * @Description: 生产者消费者模式，通过BlockingQueue实现
+ * @Author : shenyang
+ * @Date : 2015年10月8日 下午5:41:35
  */
 public class BlockingQueueStorage {
 
-	private final int MAX = 15;
-	
-	BlockingQueue<Object> bq = new LinkedBlockingQueue<Object>(MAX);
-	
-	public void enter(int i) {
+	private final int MAX = 5;
 
-		for (int num = 1; num < i; num++) {
+	private BlockingQueue<Object> bq = new LinkedBlockingQueue<Object>(MAX);
+
+	public static void main(String[] args) {
+		BlockingQueueStorage bqs = new BlockingQueueStorage();
+		new Thread(new Producer(bqs.bq, 5)).start();
+		new Thread(new Producer(bqs.bq, 10)).start();
+		new Thread(new Customer(bqs.bq, 5)).start();
+	}
+
+}
+
+class Producer implements Runnable {
+
+	private BlockingQueue<Object> bq = new LinkedBlockingQueue<Object>();
+
+	private int time;
+
+	public Producer(BlockingQueue<Object> bq, int time) {
+		this.bq = bq;
+		this.time = time;
+	}
+
+	public void run() {
+
+		while (!Thread.interrupted()) {
 			try {
 				bq.put(new Object());
+				System.out.println(Thread.currentThread().getName() + " " + bq.size());
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-
+		try {
+			TimeUnit.SECONDS.sleep(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	
-	
-	
-	
+
+}
+
+class Customer implements Runnable {
+
+	private BlockingQueue<Object> bq = new LinkedBlockingQueue<Object>();
+
+	private int time;
+
+	public Customer(BlockingQueue<Object> bq, int time) {
+		this.bq = bq;
+		this.time = time;
+	}
+
+	public void run() {
+
+		while (!Thread.interrupted()) {
+			try {
+				bq.take();
+				System.out.println(Thread.currentThread().getName() + " " + bq.size());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		try {
+			TimeUnit.SECONDS.sleep(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
