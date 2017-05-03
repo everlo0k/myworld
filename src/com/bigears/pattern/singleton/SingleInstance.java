012aaac47148a4c27f07ecb74457cb1f6b36a028
everlo0k <everlook@126.com>
2017-05-03 11:07:49 +0800

    singleton

---
+++ src/com/bigears/pattern/singleton/SingleInstance.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2001-2017 Bidlink(Beijing) E-Biz Tech Co.,Ltd.
+ * All rights reserved.
+ * 必联（北京）电子商务科技有限公司 版权所有
+ * <p>SingleInstance.java</p>
+ */
+package com.bigears.pattern.singleton;
+
+/**
+ * @Project    : myworld
+ * @ClassName  : com.bigears.pattern.singleton.SingleInstance
+ * @Description: 用单元素的枚举，实现单例模式
+ * @Author     : shenyang
+ * @Date  	   : 2017-5-3 上午11:01:52 
+ */
+public enum SingleInstance {
+
+    INSTANCE;
+    public void doSomething() {
+    }
+
+}
